724857bfab5dba263658b619f48c78a6b32d5d96
neilmackellar <neilmackellar@electrum.co.za>
2016-10-12 10:26:54 +0200

    change port

--- test/postman/run_tests.sh
+++ test/postman/run_tests.sh
@@ -3,8 +3,8 @@ basedir=$1
 testdir=$2
 echo "Starting docker container"
 docker build -t="ppu-test-server" ${basedir}/target
-docker run -d -p 8080:8080 --name ppu-test-server_container ppu-test-server
-~/circlecitools/bin/waitForServer.sh localhost:8080 5000
+docker run -d -p 9999:9999 --name ppu-test-server_container ppu-test-server
+~/circlecitools/bin/waitForServer.sh localhost:9999 5000
 ${testdir}/run_newman.sh ${testdir}
 rc=$?
 echo "Cleaning up Docker"
