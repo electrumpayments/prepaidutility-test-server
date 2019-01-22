6f41f6ede358449e385062fe25e415a54415e26f
Joshua Ramsbottom <joshuaramsbottom@electrum.co.za>
2019-01-22 16:19:54 +0200

    Update pom incl deps

--- test/postman/run_newman.sh
+++ test/postman/run_newman.sh
@@ -7,7 +7,7 @@ echo "Running newman tests:"
 newman run ${testdir}/PrepaidUtilityTestPack.postman_collection.json -e ${testdir}/PPU${host}test.postman_environment.json -d ${testdir}/test_meterIds-purchases.csv -n 10 --folder Lookup-purchase-confirm
 newman run ${testdir}/PrepaidUtilityTestPack.postman_collection.json -e ${testdir}/PPU${host}test.postman_environment.json -d ${testdir}/test_meterIds-purchases.csv -n 1 --folder Lookup-purchase-reverse
 newman run ${testdir}/PrepaidUtilityTestPack.postman_collection.json -e ${testdir}/PPU${host}test.postman_environment.json -d ${testdir}/test_meterIds-retries.csv -n 1 --folder Lookup-timeout-retry-confirm
-newman run ${testdir}/PrepaidUtilityTestPack.postman_collection.json -e ${testdir}/PPU${host}test.postman_environment.json -d ${testdir}/test_meterIds-keychange.csv -n 1 --folder "Key change"
+newman run ${testdir}/PrepaidUtilityTestPack.postman_collection.json -e ${testdir}/PPU${host}test.postman_environment.json -d ${testdir}/test_meterIds-keychange.csv -n 1 --folder "Key change requests"
 newman run ${testdir}/PrepaidUtilityTestPack.postman_collection.json -e ${testdir}/PPU${host}test.postman_environment.json -d ${testdir}/test_meterIds-keychange.csv -n 1 --folder "Fault reports"
 newman run ${testdir}/PrepaidUtilityTestPack.postman_collection.json -e ${testdir}/PPU${host}test.postman_environment.json -d ${testdir}/test_meterIds-keychange.csv -n 1 --folder "Meter lookups"
 newman run ${testdir}/PrepaidUtilityTestPack.postman_collection.json -e ${testdir}/PPU${host}test.postman_environment.json -n 1 --folder "Unmatched advices"
@@ -18,5 +18,4 @@ if [ "${?}" != 0 ]; then
 else
 	echo "Tests passed"
 	exit 0
-fi 
-
+fi
